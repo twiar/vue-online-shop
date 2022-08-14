81c18677ec7f1a99d157a9a212fd3abe3566c27a
AndreyZhitenev <zhitenev.andr@gmail.com>
2022-08-14 17:33:00 +0300

    added vuex & axios

--- src/components/v-catalog.vue
+++ src/components/v-catalog.vue
@@ -3,7 +3,7 @@
 		<h1>Catalog</h1>
 		<div class="v-catalog__list">
 			<v-catalog-item
-				v-for="product in products"
+				v-for="product in PRODUCTS"
 				:key="product.article"
 				:product_data="product"
 				@sendDataToParent="showChildArticleInConsole"
@@ -14,6 +14,8 @@
 
 <script>
 import vCatalogItem from "./v-catalog-item";
+import { mapActions, mapGetters } from "vuex";
+
 export default {
 	name: "v-catalog",
 	components: {
@@ -21,58 +23,23 @@ export default {
 	},
 	props: {},
 	data() {
-		return {
-			products: [
-				{
-					image: "1.jpg",
-					name: "T-shirt 1",
-					price: 100,
-					article: "T1",
-					available: true,
-				},
-				{
-					image: "2.jpg",
-					name: "T-shirt 2",
-					price: 150,
-					article: "T2",
-					available: true,
-				},
-				{
-					image: "3.jpg",
-					name: "T-shirt 3",
-					price: 200,
-					article: "T3",
-					available: false,
-				},
-				{
-					image: "4.jpg",
-					name: "T-shirt 4",
-					price: 300,
-					article: "T4",
-					available: true,
-				},
-				{
-					image: "5.jpg",
-					name: "T-shirt 5",
-					price: 500,
-					article: "T5",
-					available: false,
-				},
-				{
-					image: "6.jpeg",
-					name: "T-shirt 6",
-					price: 700,
-					article: "T6",
-					available: true,
-				},
-			],
-		};
+		return {};
 	},
-	computed: {},
+	computed: {
+		...mapGetters(["PRODUCTS"]),
+	},
 	methods: {
+		...mapActions(["GET_PRODUCTS_FROM_API"]),
 		showChildArticleInConsole(data) {
 			console.log(data);
 		},
+	},
+	mounted() {
+		this.GET_PRODUCTS_FROM_API().then((response) => {
+			if (response.data) {
+				console.log("Data arrived!");
+			}
+		});
 	},
 };
 </script>
